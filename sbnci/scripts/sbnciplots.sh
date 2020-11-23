9ac8f365a8b674a01a6afebfec62dd718df62147
Andrew Scarff <a.scarff@sheffield.ac.uk>
2020-11-23 03:39:31 -0600

    Actually adding modified sbnciplots.sh:x

--- sbnci/scripts/sbnciplots.sh
+++ sbnci/scripts/sbnciplots.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-##THISCIDIR=$(eval "echo \${${PROJ_PREFIX}_CI_DIR}")
-
 plotScript=`find ${FW_SEARCH_PATH//:/ } -maxdepth 1 -name sbnciplot_showervalidation.C -print -quit`
 
 root -l -b -q $plotScript\(\"${1}\"\)
 
-##root -l -b -q ${THISCIDIR}/test/CompareDataDistributions.C\(\"${DUNETPC_VERSION}\",\"${ref_dunetpc_version}\"\)
+comparisonScript=`find ${FW_SEARCH_PATH//:/ } -maxdepth 1 -name CompareDataDistributions.C -print -quit`
+
+root -l -b -q $comparisonScript\(\"${SBNDCODE_VERSION}\",\"${ref_sbndcode_version}\"\)
