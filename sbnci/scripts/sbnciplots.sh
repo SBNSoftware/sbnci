5e954a534b38cb774448041e08834f9f11b808da
Andrew Scarff <a.scarff@sheffield.ac.uk>
2020-11-30 11:15:06 -0600

    Fixing typo.

--- sbnci/scripts/sbnciplots.sh
+++ sbnci/scripts/sbnciplots.sh
@@ -4,6 +4,6 @@ plotScript="$SBNCI_DIR/scripts/sbnciplot_showervalidation.C"
 
 root -l -b -q $plotScript\(\"${1}\"\)
 
-comparisonScript="$SBNCI_Dir/scripts/CompareDataDistributions.C"
+comparisonScript="$SBNCI_DIR/scripts/CompareDataDistributions.C"
 
 root -l -b -q $comparisonScript\(\"${SBNDCODE_VERSION}\",\"${ref_sbndcode_version}\"\)
