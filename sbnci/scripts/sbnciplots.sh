76f80194af5b7333dc6e39c3f86d6d7125d49547
Andrew Scarff <a.scarff@sheffield.ac.uk>
2020-12-14 10:40:02 -0600

    Updating commands in sbnciplots.sh...again

--- sbnci/scripts/sbnciplots.sh
+++ sbnci/scripts/sbnciplots.sh
@@ -4,6 +4,20 @@ plotScript="$SBNCI_DIR/scripts/sbnciplot_showervalidation.C"
 
 root -l -b -q $plotScript\(\"${1}\"\)
 
+
+echo "ref_sbndcode_ana_hist: ${ref_sbndcode_ana_hist}"
+export ref_sbndcode_ana_hist_xrootd_path=$(echo ${ref_sbndcode_ana_hist} | sed 's#/pnfs/#root://fndca1.fnal.gov:1094/pnfs/fnal.gov/usr/#g')
+echo "ref_sbndcode_ana_hist_xrootd_path: ${ref_sbndcode_ana_hist_xrootd_path}"
+
+echo "ref_sbndcode_version: ${ref_sbndcode_version}"
+echo xrdcp -s ${ref_sbndcode_ana_hist_xrootd_path} $(basename ${ref_sbndcode_ana_hist_xrootd_path})
+xrdcp -s ${ref_sbndcode_ana_hist_xrootd_path} $(basename ${ref_sbndcode_ana_hist_xrootd_path})
+xrdcp_exitcode=$?
+echo "xrdcp_exitcode: ${xrdcp_exitcode}"
+
+mv -v ci_validation_histos.root ci_validation_histos_${ref_sbndcode_version}.root
+
+
 comparisonScript="$SBNCI_DIR/scripts/CompareDataDistributions.C"
 
 ln -v ci_validation_histos.root ci_validation_histos_${SBNDCODE_VERSION}.root
