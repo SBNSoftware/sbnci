2d65869d368837e765555d060d8d538d3e3895af
Andrew Scarff <a.scarff@sheffield.ac.uk>
2020-12-14 10:29:09 -0600

    Updating commands in sbnciplots.sh.

--- sbnci/scripts/sbnciplots.sh
+++ sbnci/scripts/sbnciplots.sh
@@ -6,4 +6,8 @@ root -l -b -q $plotScript\(\"${1}\"\)
 
 comparisonScript="$SBNCI_DIR/scripts/CompareDataDistributions.C"
 
+ln -v ci_validation_histos.root ci_validation_histos_${SBNDCODE_VERSION}.root
+
+ls -lh
+
 root -l -b -q $comparisonScript\(\"${SBNDCODE_VERSION}\",\"${ref_sbndcode_version}\"\)
