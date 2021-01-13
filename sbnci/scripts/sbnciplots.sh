e1dbdf2a7ffd47d3ecaac6871576154dc62a6a1d
Andrew Scarff <a.scarff@sheffield.ac.uk>
2021-01-13 08:57:18 -0600

    Updates to sbnciplots.sh to get correct ordering for comparison.

--- sbnci/scripts/sbnciplots.sh
+++ sbnci/scripts/sbnciplots.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+## Name of plotting script. This should be the only line that needs changing for other CI chains.
 plotScript="$SBNCI_DIR/scripts/sbnciplot_showervalidation.C"
 
-root -l -b -q $plotScript\(\"${1}\"\)
 
-
+## Setting up reference file for comparison
 echo "ref_sbndcode_ana_hist: ${ref_sbndcode_ana_hist}"
 export ref_sbndcode_ana_hist_xrootd_path=$(echo ${ref_sbndcode_ana_hist} | sed 's#/pnfs/#root://fndca1.fnal.gov:1094/pnfs/fnal.gov/usr/#g')
 echo "ref_sbndcode_ana_hist_xrootd_path: ${ref_sbndcode_ana_hist_xrootd_path}"
@@ -18,6 +18,11 @@ echo "xrdcp_exitcode: ${xrdcp_exitcode}"
 mv -v ci_validation_histos.root ci_validation_histos_${ref_sbndcode_version}.root
 
 
+## Run plotting script, producing ci_validation_histos.root
+root -l -b -q $plotScript\(\"${1}\"\)
+
+
+## Setup and run the comparison script.
 comparisonScript="$SBNCI_DIR/scripts/CompareDataDistributions.C"
 
 ln -v ci_validation_histos.root ci_validation_histos_${SBNDCODE_VERSION}.root
