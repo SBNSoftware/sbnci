70c6c6ad4e3486483fd361831f357f8dd322bc4e
Andrew Scarff <a.scarff@sheffield.ac.uk>
2021-01-14 03:29:22 -0600

    Fixing missing declarations.

--- sbnci/PlottingScripts/sbnciplot_showervalidation.C
+++ sbnci/PlottingScripts/sbnciplot_showervalidation.C
@@ -23,6 +23,9 @@ void sbnciplot_showervalidation(TString inputFile)
   TFile *inFile = TFile::Open(inputFile.Data());
   TFile *outFile = new TFile("ci_validation_histos.root", "RECREATE");
 
+  TString histName, histTitle, histXaxis, histYaxis, metricToPlot;
+  int nBins, xMin, xMax;
+
   PlotHists(inFile,  "sTrueEnergy", "True Shower Energy", "True Shower Energy", Counts, 100, 0, 2000, "sTrueEnergy_pandoraShower");
   PlotHists(inFile,  "pfpShowerHitsPurity", "Shower Hit Purity", "Purity", histYaxis, nBins, 0, 1, "pfpShowerHitsPurity_pandoraShower");
   PlotHists(inFile,  "pfpShowerHitsComp", "Shower Hit Completeness", "Completeness", histYaxis, nBins, 0, 1, "pfpShowerHitsComp_pandoraShower");
