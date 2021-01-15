52f494cbcb2f13cd104a74774cb0e0bfb4e8b8ce
Andrew Scarff <a.scarff@sheffield.ac.uk>
2021-01-15 10:03:50 -0600

    Changing the plot script to make a gDir within the .root file.

--- sbnci/PlottingScripts/sbnciplot_showervalidation.C
+++ sbnci/PlottingScripts/sbnciplot_showervalidation.C
@@ -15,7 +15,7 @@
 #include <vector>
 #include "TString.h"
 
-void PlotHists(TFile *inFile, TString histName, TString histTitle, TString histXaxis, TString histYaxis, int nBins, int xMin, int xMax, TString metricToPlot);
+void PlotHists(TFile *inFile, TFile *outFile, TString histName, TString histTitle, TString histXaxis, TString histYaxis, int nBins, int xMin, int xMax, TString metricToPlot, TString dirName);
 
 void sbnciplot_showervalidation(TString inputFile)
 {
@@ -28,35 +28,46 @@ void sbnciplot_showervalidation(TString inputFile)
   TString histYaxis = "Counts";
   int nBins = 100;
 
+  TString dirName = "pandoraShower";
 
-  PlotHists(inFile,  "sTrueEnergy", "True Shower Energy", "True Shower Energy", histYaxis, nBins, 0, 2000, "sTrueEnergy_pandoraShower");
-  PlotHists(inFile,  "pfpShowerHitsPurity", "Shower Hit Purity", "Purity", histYaxis, nBins, 0, 1, "pfpShowerHitsPurity_pandoraShower");
-  PlotHists(inFile,  "pfpShowerHitsComp", "Shower Hit Completeness", "Completeness", histYaxis, nBins, 0, 1, "pfpShowerHitsComp_pandoraShower");
-  PlotHists(inFile,  "sDirDiff", "Shower Start Direction Difference", "X", histYaxis, nBins, -1, 1, "sDirDiff_pandoraShower");
-  PlotHists(inFile,  "sStartDist", "Shower Start Position Difference", "Y", histYaxis, nBins, 0, 60, "sStartDist_pandoraShower");
-  PlotHists(inFile,  "sdEdx", "Shower Start dE/dx", "dE/dx", histYaxis, nBins, 0, 20, "sdEdx_pandoraShower");
-  PlotHists(inFile,  "sEnergy", "Shower Start Energy", "Energy", histYaxis, nBins, 0, 1500, "sEnergy_pandoraShower");
+  outFile->cd();
+  gDirectory->mkdir(dirName);
+  gSystem->Exec(Form("mkdir -v %s", dirName.Data()));
+
+
+  PlotHists(inFile, outFile, "sTrueEnergy", "True Shower Energy", "True Shower Energy", histYaxis, nBins, 0, 2000, "sTrueEnergy_pandoraShower", dirName);
+  PlotHists(inFile, outFile, "pfpShowerHitsPurity", "Shower Hit Purity", "Purity", histYaxis, nBins, 0, 1, "pfpShowerHitsPurity_pandoraShower", dirName);
+  PlotHists(inFile, outFile, "pfpShowerHitsComp", "Shower Hit Completeness", "Completeness", histYaxis, nBins, 0, 1, "pfpShowerHitsComp_pandoraShower", dirName);
+  PlotHists(inFile, outFile, "sDirDiff", "Shower Start Direction Difference", "X", histYaxis, nBins, -1, 1, "sDirDiff_pandoraShower", dirName);
+  PlotHists(inFile, outFile, "sStartDist", "Shower Start Position Difference", "Y", histYaxis, nBins, 0, 60, "sStartDist_pandoraShower", dirName);
+  PlotHists(inFile, outFile, "sdEdx", "Shower Start dE/dx", "dE/dx", histYaxis, nBins, 0, 20, "sdEdx_pandoraShower", dirName);
+  PlotHists(inFile, outFile, "sEnergy", "Shower Start Energy", "Energy", histYaxis, nBins, 0, 1500, "sEnergy_pandoraShower", dirName);
   
 }
 
 
-void PlotHists(TFile *inFile, TString histName, TString histTitle, TString histXaxis, TString histYaxis, int nBins, int xMin, int xMax, TString metricToPlot)
+void PlotHists(TFile *inFile, TFile *outFile, TString histName, TString histTitle, TString histXaxis, TString histYaxis, int nBins, int xMin, int xMax, TString metricToPlot, TString dirName)
 {
+
   TH1F* hist = new TH1F(histName, histTitle+";"+histXaxis+";"+histYaxis, nBins, xMin, xMax); 
 
   TTree* metricTree; 
   inFile->GetObject("showerValidation/MetricTree", metricTree);
   std::cout<<"metricToPlot: " << metricToPlot << ". histName: " << histName << std::endl;
   metricTree->Draw(metricToPlot + " >> " + histName);
-  
   gStyle->SetOptStat(0);
-  hist->Write();
+
+  outFile->cd();
+  outFile->cd(dirName.Data());
+
+  hist->Write("", TObject::kOverwrite);
   
   TCanvas *c1 = new TCanvas("c1","c1",800,1000);
   hist->Draw();
-  c1->Print(histName+".png");
+  c1->Print(Form("%s/%s.png", dirName.Data(), histName.Data()));
   
   delete c1;
   delete hist;
+  delete metricTree;
   return;
 }
