3e449b91b447b24c5da6bbf7e84667f841feeda9
Henry Lay <h.lay@lancaster.ac.uk>
2021-01-28 10:29:34 -0600

    Adaption to separate bnb and nue plotting workflows & implementation of range of efficiency plots

--- sbnci/scripts/recoeff_nue.sh
+++ sbnci/scripts/recoeff_nue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Name of plotting script. This should be the only line that needs changing for other CI chains.
-plotScript="$SBNCI_DIR/scripts/sbnciplot_recoeff.C"
+plotScript="$SBNCI_DIR/scripts/sbnciplot_recoeff_nue.C"
 
 ## Passing the plotting script name and the input file name to sbnciplots.sh
 source sbnciplots.sh $plotScript ${1}
