440304c3ef8ee80465b8153105721a2fa290154f
Henry Lay <h.lay@lancaster.ac.uk>
2021-02-03 05:08:39 -0600

    Naming consistency

--- sbnci/scripts/reco_tests_bnb.sh
+++ sbnci/scripts/reco_tests_bnb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Name of plotting script. This should be the only line that needs changing for other CI chains.
-plotScript="$SBNCI_DIR/scripts/sbnciplot_recoeff_nue.C"
+plotScript="$SBNCI_DIR/scripts/sbnciplot_reco_tests_bnb.C"
 
 ## Passing the plotting script name and the input file name to sbnciplots.sh
 source sbnciplots.sh $plotScript ${1}
