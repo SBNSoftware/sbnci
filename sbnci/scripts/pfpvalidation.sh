8dcc08a617e0b4ffe5d4a120364d46b5dca0fae2
Henry Lay <h.lay@lancaster.ac.uk>
2021-03-11 10:53:55 -0600

    Add bash scripts for pfp validation

---
+++ sbnci/scripts/pfpvalidation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Name of plotting script. This should be the only line that needs changing for other CI chains.
+plotScript="$SBNCI_DIR/scripts/sbnciplot_pfpvalidation.C"
+
+## Passing the plotting script name and the input file name to sbnciplots.sh
+source sbnciplots.sh $plotScript ${1}
