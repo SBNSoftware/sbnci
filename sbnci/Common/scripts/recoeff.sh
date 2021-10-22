23ded1af634e31ad660721b06e3d7335f66cb8b6
Chris Hilgenberg <chilgenb@umn.edu>
2021-10-22 11:04:38 -0500

    missed one

--- sbnci/Common/scripts/recoeff.sh
+++ sbnci/Common/scripts/recoeff.sh
@@ -5,7 +5,7 @@ export ref_sbndcode_ana_hist=${ref_recoeff_hist}
 export normalise_plots=${normalise_plots_recoeff}
 
 ## Name of plotting script. This should be the only line that needs changing for other CI chains.
-plotScript="$SBNCI_DIR/scripts/sbnciplot_recoeff.C"
+plotScript="$SBNCI_DIR/scripts/sbnciplot_recoeff_${expName}.C"
 
 ## Passing the plotting script name and the input file name to sbnciplots.sh
 source sbnciplots.sh $plotScript ${1}
