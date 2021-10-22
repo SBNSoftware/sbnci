11fc6c16516203615d92f5e5c72c078df1da3aa1
Chris Hilgenberg <chilgenb@umn.edu>
2021-10-21 20:11:58 -0500

    ...and another one (thanks @vitodb)

--- sbnci/Common/scripts/sbnci_setcodename.sh
+++ sbnci/Common/scripts/sbnci_setcodename.sh
@@ -5,12 +5,14 @@ then
   export codeName="sbndcode"
   export codeVersion=${SBNDCODE_VERSION}
   export expCIDir="${SBNCI_DIR}/SBND"
+  export expName="sbnd"
 
 elif [ ${ICARUSCODE_VERSION} ]
 then
   export codeName="icaruscode"
   export codeVersion=${ICARUSCODE_VERSION}
   export expCIDir="${SBNCI_DIR}/ICARUS"
+  export expName="icarus"
 
 else
   echo "no active version of sbndcode or icaruscode found! (should never happen)"
