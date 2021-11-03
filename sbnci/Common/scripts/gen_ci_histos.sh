f5269ebcd9b81cb4420529601a21e1049e461c93
Chris Hilgenberg <chilgenb@umn.edu>
2021-11-03 18:14:23 -0500

    add more options to target individual validation modules

--- sbnci/Common/scripts/gen_ci_histos.sh
+++ sbnci/Common/scripts/gen_ci_histos.sh
@@ -3,35 +3,89 @@
 ###################################################################
 # flag options
 
-a_flag='' # all WFs
-p_flag='' # PDS sim/reco WF
-r_flag='' # TPC reco WF
-c_flag='' # CRT sim/reco WF
-s_flag='' # TPC sim/cal WF
+all_flag='' # all WFs
+pds_flag='' # PDS sim/reco WF
+
+tpcreco_flag='' # TPC reco WF
+pfp_flag=''
+pfpslice_flag=''
+recoeff_flag=''
+shower_flag=''
+
+crt_flag='' # CRT sim/reco WF
+tpcsim_flag='' # TPC sim/cal WF
+
 
 INFILE=''
 
 print_usage() {
   printf "Usage: ./gen_ci_histos.sh <workflow options> -f <input file name>"
-  printf "\n -a all validation workflows (overrides other options)"
-  printf "\n -p PDS sim/reco validation workflow"
-  printf "\n -r TPC reco validation workflow"
-  printf "\n -c CRT sim/reco validation workflow"
-  printf "\n -s TPC sim/cal validation workflow"
+  printf "\n -a, --all         all validation workflows"
+  printf "\n -p, --pds         PDS sim/reco validation workflow"
+  printf "\n -r, --tpcreco     TPC reco validation workflow"
+  printf "\n --pfp             PFP validation workflow"
+  printf "\n --pfpslice        PFP slice validation workflow"
+  printf "\n --recoeff         TPC reco efficiency validation workflow"
+  printf "\n --shower          Shower reco efficiency validation workflow"
+  printf "\n -c, --crt         CRT sim/reco validation workflow"
+  printf "\n -s, --tpcsim      TPC sim/cal validation workflow"
+  printf "\n"
 }
 
-while getopts 'aprcsf:' flag; do
-  case "${flag}" in
-    a) a_flag='true' ;;
-    p) p_flag='true' ;;
-    r) r_flag='true' ;;
-    c) c_flag='true' ;;
-    s) s_flag='true' ;;
-    f) INFILE="${OPTARG}" ;; 
+while test $# -gt 0; do
+  case "$1" in
+    -a|--all)
+      shift
+      all_flag='true'
+      ;;
+    -c|--crt)
+      shift
+      crt_flag='true'
+      ;;
+    -p|--pds)
+      shift
+      pds_flag='true'
+      ;;
+    -s|--tpcsim)
+      shift
+      tpcsim_flag='true'
+      ;;
+    -r|--tpcreco)
+      shift
+      tpcreco_flag='true'
+      ;;
+    --pfp)
+      shift
+      pfp_flag='true'
+      ;;
+    --pfpslice)
+      shift
+      pfpslice_flag='true'
+      ;;
+    --recoeff)
+      shift
+      recoeff_flag='true'
+      ;;
+    --shower)
+      shift
+      shower_flag='true'
+      ;; 
+    -f|--file)
+      INFILE="$2"
+      shift 2
+      ;;
     *) print_usage
-       exit 1 ;;
+       exit 1
+       ;;
   esac
 done
+
+if [ $INFILE == '' ]
+then
+  echo -e "no input file provided (required)\n"
+  print_usage
+  exit 1
+fi
 
 #################################################################
 # main
@@ -41,33 +95,48 @@ source sbnci_setcodename.sh
 
 # execute plot scripts
 
-if [[ $a_flag || $p_flag ]]
+if [[ $all_flag || $pds_flag ]]
 then
   plotScript="${SBNCI_DIR}/scripts/sbnciplot_pdsvalidation_${expName}.C"
   root -l -b -q $plotScript\(\"${INFILE}\"\)
+fi
 
-elif [[ $a_flag || $r_flag ]]
+if [[ $all_flag || $tpcreco_flag ]]
 then
-  plotScript="${SBNCI_Dir}/scripts/sbnciplot_pfpvalidation_${expName}.C"
-  root -l -b -q $plotScript\(\"${INFILE}\"\)
-  
-  plotScript="$SBNCI_DIR/scripts/sbnciplot_pfpslicevalidation_${expName}.C"
-  root -l -b -q $plotScript\(\"${INFILE}\"\)
-  
-  plotScript="$SBNCI_DIR/scripts/sbnciplot_showervalidation_${expName}.C"
-  root -l -b -q $plotScript\(\"${INFILE}\"\)
-  
-  plotScript="$SBNCI_DIR/scripts/sbnciplot_recoeff_${expName}.C"
-  root -l -b -q $plotScript\(\"${INFILE}\"\)
+  if [[ $tpcreco_flag || $pfp_flag ]]
+  then
+    plotScript="${SBNCI_Dir}/scripts/sbnciplot_pfpvalidation_${expName}.C"
+    root -l -b -q $plotScript\(\"${INFILE}\"\)
+  fi
 
-elif [[ $a_flag || $c_flag ]]
+  if [[ $tpcreco_flag || $pfpslice_flag ]]
+  then
+    plotScript="$SBNCI_DIR/scripts/sbnciplot_pfpslicevalidation_${expName}.C"
+    root -l -b -q $plotScript\(\"${INFILE}\"\)
+  fi
+
+  if [[ $tpcreco_flag || $shower_flag ]]
+  then
+    plotScript="$SBNCI_DIR/scripts/sbnciplot_showervalidation_${expName}.C"
+    root -l -b -q $plotScript\(\"${INFILE}\"\)
+  fi
+
+  if [[ $tpcreco_flag || $recoeff_flag ]]
+  then
+    plotScript="$SBNCI_DIR/scripts/sbnciplot_recoeff_${expName}.C"
+    root -l -b -q $plotScript\(\"${INFILE}\"\)
+  fi
+fi
+
+if [[ $all_flag || $crt_flag ]]
 then
   plotScript="${SBNCI_DIR}/scripts/sbnciplot_crtvalidation_${expName}.C"
   root -l -b -q $plotScript\(\"${INFILE}\"\)
+fi
 
-elif [[ $a_flag || $s_flag ]]
+if [[ $all_flag || $tpcsim_flag ]]
 then
   plotScript="${SBNCI_DIR}/scripts/sbnciplot_tpcsimvalidation_${expName}.C"
   root -l -b -q $plotScript\(\"${INFILE}\"\)
-
 fi
+
