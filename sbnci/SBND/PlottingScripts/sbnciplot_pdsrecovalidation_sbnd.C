2b36514d51528b06a21d931857a6d794470fe4da
Chris Hilgenberg <chilgenb@umn.edu>
2022-05-11 15:24:52 -0500

    fix tree name

--- sbnci/SBND/PlottingScripts/sbnciplot_pdsrecovalidation_sbnd.C
+++ sbnci/SBND/PlottingScripts/sbnciplot_pdsrecovalidation_sbnd.C
@@ -37,28 +37,28 @@ void sbnciplot_pdsrecovalidation_sbnd(TString inputFile)
   int nBinsPE=25;
   int nBinsPEOpCh=60;
 
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "PMTNHit","", "PMTNHit", histYaxis, nBins, 0, 5000, "PMTNHit", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "PMTPeakTime","", "PMTPeakTime-NuG4Time [#mus]", histYaxis, 200, -10, 40, "PMTPeakTime", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "PMTWidth","", "PMTWidth [#mus] ", histYaxis, nBins, 0, 3, "PMTWidth", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "PMTAmplitude","", "PMTAmplitude [ADC]", histYaxis, nBins, 0, 3050, "PMTAmplitude", "", dirName); //XRange upper value set to PMT ADC saturation value
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "PMTPE","", "PMTPE", histYaxis, nBinsPEOpCh, 0, 10000, "PMTPE", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "PMTPEResolutionVec","", "(PMTPE-PMTSimPhot)/PMTSimPhot (per OpCh)", histYaxis, nBins, -1, 1.5, "PMTPEResolutionVec", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "PMTNHit","", "PMTNHit", histYaxis, nBins, 0, 5000, "PMTNHit", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "PMTPeakTime","", "PMTPeakTime-NuG4Time [#mus]", histYaxis, 200, -10, 40, "PMTPeakTime", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "PMTWidth","", "PMTWidth [#mus] ", histYaxis, nBins, 0, 3, "PMTWidth", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "PMTAmplitude","", "PMTAmplitude [ADC]", histYaxis, nBins, 0, 3050, "PMTAmplitude", "", dirName); //XRange upper value set to PMT ADC saturation value
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "PMTPE","", "PMTPE", histYaxis, nBinsPEOpCh, 0, 10000, "PMTPE", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "PMTPEResolutionVec","", "(PMTPE-PMTSimPhot)/PMTSimPhot (per OpCh)", histYaxis, nBins, -1, 1.5, "PMTPEResolutionVec", "", dirName);
 
 
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "ArapucaNHit","", "ArapucaNHit", histYaxis, nBins, 0, 500, "ArapucaNHit", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "ArapucaPeakTime","", "ArapucaPeakTime", histYaxis, nBins, 0, 10, "ArapucaPeakTime", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "ArapucaWidth","", "ArapucaWidth", histYaxis, nBins, 0, 5, "ArapucaWidth", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "ArapucaAmplitude","", "ArapucaAmplitude", histYaxis, nBins, 0, 4000, "ArapucaAmplitude", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "ArapucaPE","", "ArapucaPE", histYaxis, nBinsPEOpCh, 0, 500, "ArapucaPE", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "ArapucaPEResolutionVec","", "(ArapucaPE-ArapucaSimPhot)/ArapucaSimPhot", histYaxis, nBins, -1, 1.5, "ArapucaPEResolutionVec", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "ArapucaNHit","", "ArapucaNHit", histYaxis, nBins, 0, 500, "ArapucaNHit", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "ArapucaPeakTime","", "ArapucaPeakTime", histYaxis, nBins, 0, 10, "ArapucaPeakTime", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "ArapucaWidth","", "ArapucaWidth", histYaxis, nBins, 0, 5, "ArapucaWidth", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "ArapucaAmplitude","", "ArapucaAmplitude", histYaxis, nBins, 0, 4000, "ArapucaAmplitude", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "ArapucaPE","", "ArapucaPE", histYaxis, nBinsPEOpCh, 0, 500, "ArapucaPE", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "ArapucaPEResolutionVec","", "(ArapucaPE-ArapucaSimPhot)/ArapucaSimPhot", histYaxis, nBins, -1, 1.5, "ArapucaPEResolutionVec", "", dirName);
 
 
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "NFlash","", "NFlash", histYaxis, 10, 0, 10, "NFlash", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "TotalFlashPE","", "TotalFlashPE", histYaxis, nBinsPE, 0,50000, "TotalFlashPE", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "FlashTime","", "FlashTime [#mus]", histYaxis, 20, 0, 2, "FlashTime", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "FlashTimeResolution","", "FlashTime-NuG4Time [ns]", histYaxis, nBins, -0.05, 0.1, "FlashTimeResolution", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "YCenter","", "YCenter [cm]", histYaxis, 20, -200, 200, "YCenter", "", dirName);
-  PlotHists(inFile, outFile, "pdsrecovalidation/pdsTree", "ZCenter","", "ZCenter [cm]", histYaxis, 25, 0, 500, "ZCenter", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "NFlash","", "NFlash", histYaxis, 10, 0, 10, "NFlash", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "TotalFlashPE","", "TotalFlashPE", histYaxis, nBinsPE, 0,50000, "TotalFlashPE", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "FlashTime","", "FlashTime [#mus]", histYaxis, 20, 0, 2, "FlashTime", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "FlashTimeResolution","", "FlashTime-NuG4Time [ns]", histYaxis, nBins, -0.05, 0.1, "FlashTimeResolution", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "YCenter","", "YCenter [cm]", histYaxis, 20, -200, 200, "YCenter", "", dirName);
+  PlotHists(inFile, outFile, "pdsrecovalidation/pdsRecoTree", "ZCenter","", "ZCenter [cm]", histYaxis, 25, 0, 500, "ZCenter", "", dirName);
 
 }
 
