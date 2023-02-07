206077c7bb3abbca35fac2f32658d5c39211b147
Iker de Icaza Astiz <icaza@fnal.gov>
2023-02-07 17:06:15 +0000

    Test new pds sim photons propagation time distribution

--- sbnci/SBND/PlottingScripts/sbnciplot_pdssimvalidation_sbnd.C
+++ sbnci/SBND/PlottingScripts/sbnciplot_pdssimvalidation_sbnd.C
@@ -15,7 +15,17 @@
 #include <vector>
 #include "TString.h"
 
-void PlotHists(TFile *inFile, TFile *outFile, TString treeName, TString histName, TString histTitle, TString histXaxis, TString histYaxis, int nBins, double xMin, double xMax, TString metricToPlot, TString condition, TString dirName);
+void PlotHists(TFile *inFile, TFile *outFile,
+               TString treeName, TString histName, TString histTitle,
+               TString histXaxis, TString histYaxis,
+               int nBins, double xMin, double xMax,
+               TString metricToPlot, TString condition, TString dirName);
+void PlotMapHists(TFile *inFile, TFile *outFile,
+                  TString treeName, TString histName, TString histTitle,
+                  TString histXaxis, TString histYaxis,
+                  int nBins, double xMin, double xMax,
+                  TString metricToPlot, TString condition, TString dirName)
+
 
 void sbnciplot_pdssimvalidation_sbnd(TString inputFile)
 {
@@ -23,37 +33,78 @@ void sbnciplot_pdssimvalidation_sbnd(TString inputFile)
   TFile *inFile = TFile::Open(inputFile.Data());
   TFile *outFile = new TFile("ci_validation_histos.root", "RECREATE");
 
-  TString histName, histTitle, histXaxis, metricToPlot;
-  int xMin, xMax;
   TString histYaxis = "Counts";
-  int nBins = 50;
-
   TString dirName = "pdssimvalidation";
+  int nBinsPE = 25;
+  int nBinsPEOpCh = 50;
 
   outFile->cd();
   gDirectory->mkdir(dirName);
   gSystem->Exec(Form("mkdir -v %s", dirName.Data()));
 
-  int nBinsPE=25;
-  int nBinsPEOpCh=60;
-  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsVec","", "#SimPhotons (per OpCh)", histYaxis, nBinsPEOpCh, 0, 10000, "NPhotonsVec", "", dirName);
-  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotons","", "#SimPhotons", histYaxis, nBinsPE, 0, 60000, "NPhotons", "", dirName);
-  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsPMTCoated","", "#SimPhotons_{CoatedPMTs}", histYaxis, nBinsPE, 0, 40000, "NPhotonsPMTCoated", "", dirName);
-  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsPMTUncoated","", "#SimPhotons_{UncoatedPMTs}", histYaxis, nBinsPE, 0, 5000, "NPhotonsPMTUncoated", "", dirName);
-  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsXARAPUCAVuv","", "#SimPhotons_{XARAPUCAVuv}", histYaxis, nBinsPE, 0, 5000, "NPhotonsXARAPUCAVuv", "", dirName);
-  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsXARAPUCAVis","", "#SimPhotons_{XARAPUCAVis}", histYaxis, nBinsPE, 0, 5000, "NPhotonsXARAPUCAVis", "", dirName);
+  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsVec",         "", "#SimPhotons (per OpCh)",    histYaxis, nBinsPEOpCh, 0, 1500, "NPhotonsVec", "", dirName);
+  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotons",            "", "#SimPhotons",               histYaxis, nBinsPE, 0, 60000, "NPhotons", "", dirName);
+  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsPMTCoated",   "", "#SimPhotons_{CoatedPMTs}",  histYaxis, nBinsPE, 0, 40000, "NPhotonsPMTCoated", "", dirName);
+  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsPMTUncoated", "", "#SimPhotons_{UncoatdPMTs}", histYaxis, nBinsPE, 0, 5000, "NPhotonsPMTUncoated", "", dirName);
+  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsXARAPUCAVuv", "", "#SimPhotons_{XARAPUCAVuv}", histYaxis, nBinsPE, 0, 5000, "NPhotonsXARAPUCAVuv", "", dirName);
+  PlotHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NPhotonsXARAPUCAVis", "", "#SimPhotons_{XARAPUCAVis}", histYaxis, nBinsPE, 0, 5000, "NPhotonsXARAPUCAVis", "", dirName);
+  PlotMapHists(inFile, outFile, "pdssimvalidation/pdsSimTree", "NTimePhotonsMap",  "", "Photons Arrival Time - T0 [ns]",    histYaxis, nBinsPEOpCh, -200, 800, "NPhotonsVec", "", dirName);
 
 }
 
-void PlotHists(TFile *inFile, TFile *outFile, TString treeName, TString histName, TString histTitle, TString histXaxis, TString histYaxis, int nBins, double xMin, double xMax, TString metricToPlot, TString condition, TString dirName)
+void PlotHists(TFile *inFile, TFile *outFile,
+               TString treeName, TString histName, TString histTitle,
+               TString histXaxis, TString histYaxis,
+               int nBins, double xMin, double xMax,
+               TString metricToPlot, TString condition, TString dirName)
 {
 
   TH1F* hist = new TH1F(histName, histTitle+";"+histXaxis+";"+histYaxis, nBins, xMin, xMax);
 
   TTree* metricTree;
   inFile->GetObject(treeName, metricTree);
-  std::cout<<"metricToPlot: " << metricToPlot << ". histName: " << histName << std::endl;
+  std::cout << "metricToPlot: " << metricToPlot
+            << ". histName: " << histName
+            << ". condition: " << condition << std::endl;
   metricTree->Draw(metricToPlot + " >> " + histName, condition);
+  gStyle->SetOptStat(0);
+
+  outFile->cd();
+  outFile->cd(dirName.Data());
+
+  hist->Write("", TObject::kOverwrite);
+
+  TCanvas *c1 = new TCanvas("c1","c1",800,1000);
+  hist->Draw();
+  c1->Print(Form("%s/%s.png", dirName.Data(), histName.Data()));
+
+  delete c1;
+  delete hist;
+  delete metricTree;
+  return;
+}
+
+void PlotMapHists(TFile *inFile, TFile *outFile,
+                  TString treeName, TString histName, TString histTitle,
+                  TString histXaxis, TString histYaxis,
+                  int nBins, double xMin, double xMax,
+                  TString metricToPlot, TString condition, TString dirName)
+{
+
+  TH1F* hist = new TH1F(histName, histTitle+";"+histXaxis+";"+histYaxis, nBins, xMin, xMax);
+
+  std::map<int,int>* nTimePhotonsMap_p;
+  infile->GetObject("NTimePhotonsMap", nTimePhotonsMap_p);
+
+  for(auto& ntp : nTimePhotonsMap_p) {
+    hist->Fill(ntp->first, ntp->second);
+  }
+  // TTree* metricTree;
+  // inFile->GetObject(treeName, metricTree);
+  // std::cout << "metricToPlot: " << metricToPlot
+  //           << ". histName: " << histName
+  //           << ". condition: " << condition << std::endl;
+  // metricTree->Draw(metricToPlot + " >> " + histName, condition);
   gStyle->SetOptStat(0);
 
   outFile->cd();
